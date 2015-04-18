ff6d69e43b5fd305e652b87ee1a71b4f3f70429a
Krishna Glick <unstoppable411@gmail.com>
2015-04-17 22:57:39 -0400

    Minor bugfix, improved my-points listing.

--- app/routes/point-me/login.js
+++ app/routes/point-me/login.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	setupController: function(controller) {
 		controller.set('session.appTitle', 'Login');
+		controller.set('isRegistering', false);
 	}
 });
