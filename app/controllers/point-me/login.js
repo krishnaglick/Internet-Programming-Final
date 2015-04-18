86074ac8a9632ed7258c9ce634855ef722c0c033
Krishna Glick <unstoppable411@gmail.com>
2015-04-18 16:48:44 -0400

    Can now use enter to login! Also improved UX on login/register page.

--- app/controllers/point-me/login.js
+++ app/controllers/point-me/login.js
@@ -9,30 +9,42 @@ var loginController = Ember.Controller.extend({
 		password: '',
 		email: ''
 	},
+    canLogin: function() {
+        return  this.get('user.username') &&
+                this.get('user.password');
+    }.property('user.username', 'user.password'),
+    canRegister: function() {
+        return  this.get('canLogin')    &&
+                this.get('user.email')  ;
+    }.property('canLogin', 'user.email'),
 	actions: {
         login: function() {
-        	this.set('loading', 'loading');
-            var authXHR = this.get('authenticationService').login(this.user);
-            if(authXHR) {
-            	authXHR.success((data) => {
-            		this.get('authenticationService').createSession(this, data);
-            	});
-            	authXHR.complete(() => {
-            		this.set('loading', '');
-            	});
-			}
+            if(this.get('canLogin')) {
+            	this.set('loading', 'loading');
+                var authXHR = this.get('authenticationService').login(this.user);
+                if(authXHR) {
+                	authXHR.success((data) => {
+                		this.get('authenticationService').createSession(this, data);
+                	});
+                	authXHR.complete(() => {
+                		this.set('loading', '');
+                	});
+    			}
+            }
         },
         register: function() {
-        	this.set('loading', 'loading');
-            var authXHR = this.get('authenticationService').register(this.user);
-            if(authXHR) {
-            	authXHR.success((data) => {
-            		this.get('authenticationService').createSession(this, data);
-            	});
-            	authXHR.complete(() => {
-            		this.set('loading', '');
-            	});
-			}
+            if(this.get('canRegister')) {
+            	this.set('loading', 'loading');
+                var authXHR = this.get('authenticationService').register(this.user);
+                if(authXHR) {
+                	authXHR.success((data) => {
+                		this.get('authenticationService').createSession(this, data);
+                	});
+                	authXHR.complete(() => {
+                		this.set('loading', '');
+                	});
+    			}
+            }
         },
 		createAccount: function() {
 			this.set('isRegistering', true);
@@ -41,7 +53,15 @@ var loginController = Ember.Controller.extend({
 		cancelCreateAccount: function() {
 			this.set('isRegistering', false);
 			this.set('session.appTitle', 'Login');
-		}
+		},
+        enterAction: function() {
+            if(this.get('isRegistering')) {
+                this.send('register');
+            }
+            else {
+                this.send('login');
+            }
+        }
 	}
 });
 
